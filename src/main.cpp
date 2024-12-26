924d299f3788fec400a7c4af8b944e03cde88122
Manfred Gawlas <manfredgawlas12@gmail.com>
2024-12-26 20:00:00 +0000

    FIRE

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,113 @@
 #include <chrono>
 #include "sc2actions.hpp"
 #include <iostream>
+#include <thread>
+#include <atomic>
+#include <mutex>
 
-bool exit_signal = false;
+std::atomic<bool> exit_signal(false);
+std::atomic<int> hits(0);
+std::mutex action_mutex;
 
 void signal_handler(int signum) {
     if (signum == SIGINT) {
         exit_signal = true;
     }
 }
+
+void animate(const sc2action& action, std::chrono::steady_clock::time_point start_time) {
+    start_color();
+    init_pair(1, COLOR_RED, COLOR_BLACK);
+    init_pair(2, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(3, COLOR_WHITE, COLOR_BLACK);
+
+    const std::vector<std::vector<std::string>> fire_frames = {
+        {
+            "    #   #   #    #  ## ",
+            "   ##  # #  ##  # ### #",
+            "   ### # #  ## # ### ##",
+            "    #### ## ### ### ####",
+            "    ###################",
+            "    #                 #",
+            "    #     APM: %d     #",
+            "    #                 #",
+            "    ###################"
+        },
+        {
+            "   #  # #  # # # #  ## ",
+            "   ##  ## ## ## ### # #",
+            "   # ## #  ## # ### ### ",
+            " #  ##### ### # ### ####",
+            " ## #################### #",
+            "    #                 #",
+            "    #     APM: %d     # #",
+            "   ##                 #",
+            "    ###################"
+        },
+        {
+            " ##  # # #    #   #  # #",
+            "   ##  ## ## ## ###  ###",
+            "   # ## #  ## # # # ### #",
+            " #  ## # #### # ##### ## #",
+            " ## #################### #",
+            "   ##                 ###",
+            "   ##     APM: %d     #",
+            "   ##                 ##",
+            "    ###################"
+        }
+    };
+
+    int frame_id = 0;
+    int counter = 0;
+    while (!exit_signal) {
+        int hits_local = hits.load();
+
+        const auto end_time = std::chrono::steady_clock::now();
+        const std::chrono::duration<double> total_time = end_time - start_time;
+        const double total_time_sec = total_time.count();
+        const double total_time_min = total_time_sec / 60;
+
+
+        const int apm = static_cast<int>(hits_local / total_time_min);
+
+        clear();
+
+
+        for (int i = 0; i < (int)fire_frames[frame_id].size(); ++i) {
+            if (i % 2 == 0) {
+                attron(COLOR_PAIR(1));
+            } else {
+                attron(COLOR_PAIR(2));
+            }
+            mvprintw(i + 5, 0, fire_frames[frame_id][i].c_str(), apm);
+            if (i % 2 == 0) {
+                attroff(COLOR_PAIR(1));
+            } else {
+                attroff(COLOR_PAIR(2));
+            }
+        }
+
+        attron(COLOR_PAIR(3));
+        mvprintw(0, 0, "WELCOM TO SC2 APM TRAINING BUAHAHAHAHA");
+        attroff(COLOR_PAIR(3));
+
+        action_mutex.lock();
+        mvprintw((int)fire_frames[frame_id].size() + 10, 0, "ACTION: %s", action.desc.c_str());
+        mvprintw((int)fire_frames[frame_id].size() + 11, 0, "HOTKEY: %c", action.display_key);
+        action_mutex.unlock();
+
+        refresh();
+
+        if (counter == 10) {
+            frame_id = (frame_id + 1) % (int)fire_frames.size();
+            counter = 0;
+        }
+        else counter++;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(20));
+    }
+}
+
 
 int main() {
     signal(SIGINT, signal_handler);
@@ -28,21 +127,13 @@ int main() {
 
     std::vector<sc2action> actions = init_actions();
 
-    int hits = 0;
+    sc2action action = get_random_action(actions);
 
-    // Thanks god we have auto
     const auto start_time = std::chrono::steady_clock::now();
+    std::thread animation_thread(animate, std::ref(action), start_time);
+    // Thanks god we have auto
 
     while (!exit_signal) {
-        clear();
-        mvprintw(0, 0, "WELCOM TO SC2 APM TRAINING BUAHAHAHAHA");
-        mvprintw(1, 0, "HITS: %d", hits);
-
-        sc2action action = get_random_action(actions);
-        mvprintw(3, 0, "ACTION: %s", action.desc.c_str());
-        mvprintw(4, 0, "HOTKEY: %c", action.display_key);
-
-        refresh();
         int input = getch();
 
         if (input == ':') {
@@ -53,8 +144,13 @@ int main() {
             }
         }
 
+        // read-read is no problemo
         if (input == action.key) {
             hits++;
+
+            action_mutex.lock();
+            action = get_random_action(actions);
+            action_mutex.unlock();
         }
     }
 
